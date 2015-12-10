b56712b991562719175a4db37fa53702d2c8c9e6
manelone <minymoh@gmail.com>
2015-12-10 13:21:21 -0800

    oracle algorithm
    is not working. returns shorter path with higher crime

--- oracle.py
+++ oracle.py
@@ -4,8 +4,8 @@ import edge_connects, math, random, collections, heapq, re, sys, time, os, datet
 
 nodeDict = edge_connects.nodeDict()
 
-#dictionary from edges to list of times of known crimes (crime type, time) at that edge
-knownCrimes = edge_connects.readKnownCrimes()
+#dictionary from crimeStreets to list of times of known crimes (crime type, time) at that edge
+#knownCrimes = edge_connects.readKnownCrimes()
 
 RISK_WEIGHT = 4
 LENGTH_WEIGHT = 1
@@ -13,13 +13,13 @@ LENGTH_WEIGHT = 1
 
 #returns true if any times in the given list of times fall within the range of 
 #of the given time t
-def inRange(t, times):
-	start = t - datetime.timedelta(minutes=30)
-	end = t + datetime.timedelta(minutes=60)
-	for tm in times:
-		if tm[1] >= start and tm[1] <= end:
-			return True
-	return False
+# def inRange(t, times):
+# 	start = t - datetime.timedelta(minutes=30)
+# 	end = t + datetime.timedelta(minutes=60)
+# 	for tm in times:
+# 		if tm[1] >= start and tm[1] <= end:
+# 			return True
+# 	return False
 
 class Journey():
 	def __init__(self, path, startTime) :
@@ -48,6 +48,12 @@ class Journey():
  		crimeScore = 0
  		for street in self.path:
  			crimeScore += street.getCrimeScore()
+ 		return crimeScore
+ 	
+ 	def getKnownCrimeScore(self):
+ 		crimeScore = 0
+ 		for street in self.path:
+ 			crimeScore += street.knownCrimeScore(self.startTime)
  		return crimeScore
 
  	def printPath(self):
@@ -143,14 +149,15 @@ def aStarSearch(start, end, startTime):
 				#Heuristic: manhattan distance from currNode to endNode
 				distance = math.sqrt((end[0] - newNode [0])*(end[0] - newNode [0]) + (end[1] - newNode [1])*(end[1] - newNode [1]))
 				
-				if street in knownCrimes and inRange(startTime, knownCrimes[street]):
-					crimeScore =100
+				if street.knownCrimeScore(startTime) != 0:
+					risk = 10**12
 				else:
-					crimeScore = 0
+					risk = 1
 
-				print ('edge: ' + str(street.edgeID) + ' score: ' + str(crimeScore))
-				cost = math.e**(crimeScore * RISK_WEIGHT)*street.st_length**LENGTH_WEIGHT + distance**LENGTH_WEIGHT
 				
+				cost = (risk)*street.st_length**LENGTH_WEIGHT + distance**LENGTH_WEIGHT
+				#cost = risk
+				print ('edge: ' + str(street.edgeID) + ' cost: ' + str(cost))
 				if pq.update(newNode, cost):
 					parentage[newNode] = street
 	
@@ -158,7 +165,7 @@ def aStarSearch(start, end, startTime):
 	return None
 
 
-startTime = time.strptime('Friday,10/16/2015,22:00', "%A,%m/%d/%Y,%H:%M")
+startTime = time.strptime('Friday,10/31/2014,23:30', "%A,%m/%d/%Y,%H:%M")
 formattedStartTime = datetime.datetime.fromtimestamp(time.mktime(startTime))
 journey = aStarSearch((37.796028, -122.44310800000001),(37.781566999999995, -122.41133899999998), formattedStartTime)
 print ('here\'s our path')
@@ -166,6 +173,6 @@ journey.printPath()
 # print(journey.path)
 print('length: '+ str(journey.getLength()))
 print('total crimes: '+ str(journey.getNumCrimes()))
-print('total crime score: '+ str(journey.getTotalCrimeScore()))
+print('total crime score: '+ str(journey.getKnownCrimeScore()))
 
 
