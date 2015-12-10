3d05cc6be18f1a3d78e57155420f6579ecab63bd
Joseph Shayani <Joseph@DN0a2272af.SUNet>
2015-12-10 12:09:43 -0800

    pre-switch to need road network

--- trim_edges_further.py
+++ trim_edges_further.py
@@ -5,12 +5,18 @@ filename = "trimmed_edges.csv"
 
 edges = pd.read_csv(filename)
 
+lines = tuple(open(filename, 'r'))
+
+fout = open('sf_edges','w')
+
 i = 0
-for line in fileinput.input(filename, inplace=True):
-	e = edges[i][1]
+line = lines[i]
+fout.write(line)
+for edge in edges.iterrows():
+	i += 1
+	e = edge[1]
 	startCoords = eval(e['startCoords'])
 	endCoords = eval(e['endCoords'])
-	i += 1
 	if startCoords[1] > -122.35 or startCoords[1] < -122.52: 
 		continue
 	if startCoords[0] > 37.835 or startCoords[0] < 37.7: # changing these numbers to be more refined to SF
@@ -19,4 +25,7 @@ for line in fileinput.input(filename, inplace=True):
 		continue
 	if endCoords[0] > 37.835 or endCoords[0] < 37.7:
 		continue
-	print line,
+	line = lines[i]
+	fout.write(line)
+
+fout.close()
